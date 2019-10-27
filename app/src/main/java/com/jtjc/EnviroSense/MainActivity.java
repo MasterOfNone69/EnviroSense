ff264beffee76f9fc72aaef442b2a7708f179c8e
tristinxie <53798468+tristinxie@users.noreply.github.com>
2019-10-26 22:02:44 -0700

    Firebase set up

--- app/src/main/java/com/jtjc/EnviroSense/MainActivity.java
+++ app/src/main/java/com/jtjc/EnviroSense/MainActivity.java
@@ -1,17 +1,51 @@
 package com.jtjc.EnviroSense;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.sql.Ref;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);}
+        setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("t1", "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("t2", "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+
 
     public void startGraph(View view) {
         android.content.Intent graph1 = new Intent(this, TempGraph.class);
